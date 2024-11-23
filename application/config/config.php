e64d20e297aa582be767d99589d946e5b34e9385
bharat-l <softtechbharatl@gmial.com>
2024-11-23 10:57:18 +0530

    add forgot page and change styles

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost/student/';
-$config['login_url'] = 'LoginController';
+// $config['login_url'] = 'LoginController';
 /*
 |--------------------------------------------------------------------------
 | Index File
